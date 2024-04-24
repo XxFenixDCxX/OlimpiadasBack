5c9310ad4f4216bb835de539b562a2b7ce54e865
xxSTUX <aminbensbaho@gmail.com>
2024-04-24 09:45:09 +0200

    Add Event entity to PurchasesController

--- src/Controller/PurchasesController.php
+++ src/Controller/PurchasesController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\AuthService;
 use App\Controller\AuthController;
+use App\Entity\Event;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PurchasesController extends AbstractController
